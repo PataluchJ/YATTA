2a6a674ae3249f0293dc18b4301fad825f170ad1
Revek312 <93083745+PataluchJ@users.noreply.github.com>
2022-02-04 12:40:04 +0100

    Command test.

--- Backend/wrapper_test.py
+++ Backend/wrapper_test.py
@@ -10,27 +10,9 @@ class TestWrapper(unittest.TestCase):
 
     # Message tests
 
-    def test_chat(self):
-        f = open('Test_Records/messages.json')
-        messages = json.load(f)
-        f.close()
-
-        # All message test
-        all_message_list = {'Messages' : messages}
-        self.assertEqual(all_message_list, json.loads(wp.chat_messages_all()), 'Get all messages.')
-        
-        # Message by ID
-        # Get
-        id = 2
-        id_message_list = {'Messages': [messages[id]]}
-        id_message_arg = json.dumps({"Id": id})
-        self.assertEqual(id_message_list, json.loads(wp.chat_message_by_id(id_message_arg)), 'Get by id message.')
-
-        # Message since 
-        id = 3
-        since_message_list = {'Messages': messages[3:]}
-        since_message_arg = json.dumps({"Id": 3})
-        self.assertEqual(since_message_list, json.loads(wp.chat_messages_since(since_message_arg)), 'Get messages since.')
+    def test_commands(self):
+        r = wp.chat_command_exec({'User':'Test', 'Character':'Test', 'Command':'/roll 1d1', 'Room':'None'})
+        self.assertEqual('Rolled:1', r['Text'])
 
 if __name__ == '__main__':
     unittest.main()
